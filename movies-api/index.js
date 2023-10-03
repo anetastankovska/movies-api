f11d3ec2feb9ff0b32ad22aea2fd10b8b065f0d6
anetastankovska <aneta.stankovska@cw.bluedotcorp.com>
2023-10-04 00:05:58 +0200

    set server side config for CORS headers

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,6 +6,13 @@ const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 app.use("/api", globalRouter);
