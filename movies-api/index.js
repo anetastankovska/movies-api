c6c7e7ab70309b86be4e85cf777af4df1632fbd9
anetastankovska <aneta.stankovska@cw.bluedotcorp.com>
2023-10-12 13:43:54 +0200

    configure headers for CORS

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,8 +7,9 @@ const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
